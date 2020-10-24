41bb6ce53b51757b5f4c43707f49dfc1f2113324
Gabriel Blanco <geblarot96@gmail.com>
2020-10-24 13:09:49 -0500

    Cambios Finales

--- GabrielBlancoAlbertoOrtega_JDBC/src/co/edu/unbosque/model/MyDatabase.java
+++ GabrielBlancoAlbertoOrtega_JDBC/src/co/edu/unbosque/model/MyDatabase.java
@@ -11,19 +11,6 @@ public class MyDatabase {
 	private Connection con;
 	private ResultSet rs;
 	private Statement statement;
-
-	public MyDatabase() {
-		/*try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			String user = "fdpK9Mq4Lh";
-			String pass = "LPZhQYsOeL";
-			con = DriverManager.getConnection("jdbc:mysql://remotemysql.com:3306/" + user, user, pass);
-			statement = con.createStatement();
-			con = DriverManager.getConnection("jdbc:mysql://remotemysql.com:3306/" + user, user, pass);
-		} catch (ClassNotFoundException | SQLException e) {
-			System.out.println("Check connection");
-		}*/
-	}
 	
 	public String getAllData() {
 		var msg = "";
@@ -35,7 +22,18 @@ public class MyDatabase {
 			statement = con.createStatement();
 			rs = statement.executeQuery("SELECT * FROM datos");
 			while(rs.next()) {
-				msg += rs.getString(1)+";"+rs.getString(2)+";"+rs.getString(3)+";"+rs.getString(4)+";"+rs.getString(5)+";"+rs.getString(6)+";"+rs.getString(7)+";"+rs.getString(8)+";"+rs.getString(9)+";"+rs.getString(10)+";"+rs.getString(11)+";"+rs.getString(12)+"\n";
+				msg += rs.getString(1)+
+						";"+rs.getString(2)+
+						";"+rs.getString(3)+
+						";"+rs.getString(4)+
+						";"+rs.getString(5)+
+						";"+rs.getString(6)+
+						";"+rs.getString(7)+
+						";"+rs.getString(8)+
+						";"+rs.getString(9)+
+						";"+rs.getString(10)+
+						";"+rs.getString(11)+
+						";"+rs.getString(12)+"\n";
 			}
 		} catch (SQLException | ClassNotFoundException e) {
 			e.printStackTrace();
